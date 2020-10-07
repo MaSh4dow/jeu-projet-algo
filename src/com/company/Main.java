77fc7fce53e8b216bdcca6966c46120776d6cc3d
Shadow <kcoudray2001@gmail.com>
2020-10-07 14:35:43 +0200

    Création du menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // création du menu
-        Scanner scanner = new Scanner (System.in);
-        String menuJeu = new String();
-        String menuRègle = new String();
-        String menuQuitterLeJeu = new String();
-        System.out.println("1- Pour jouer écris : Jouer");
-        menuJeu = scanner.next();
-        System.out.println("2- Pour lire les règles écris : règles");
-        menuRègle = scanner.next();
-        System.out.println("c'est moi");
+        Scanner scanner = new Scanner(System.in);
+        String choixMenuPrincipal;
 
+        System.out.println("Bienvenue sur notre jeu intéractif. Tapez le chiffre correspondant en fonction de ce que vous voulez faire :");
+        System.out.println(); // Sauter une ligne
+        System.out.println("    -1 Jouer"); // Tape 1 si il veut lancer une partie
+        System.out.println("    -2 Voir les règles"); // Tape 2 si il veut voir les règles
+        System.out.println("    -0 Quitter le jeu"); // Tape 0 si il veut quitter le jeu
+
+        choixMenuPrincipal = scanner.nextLine(); // Choix de l'utilisateur
+
+        while (!(choixMenuPrincipal.equals(1)) && !(choixMenuPrincipal.equals(2)) && !(choixMenuPrincipal.equals(0))){ // Si la saisie du choix est incorrecte
+            System.out.println("Votre saisie est incorrecte. Tapez le chiffre correspondant en fonction de ce que vous voulez faire");
+            System.out.println();
+            System.out.println("    -1 Jouer");
+            System.out.println("    -2 Voir les règles");
+            System.out.println("    -0 Quitter le jeu");
+
+            choixMenuPrincipal = scanner.nextLine();
+        }
+
+        if (choixMenuPrincipal.equals(1)) {
+            //Implémenter la fonction pour jouer
+            System.out.println("Implémenter la fonction pour jouer");
+        }
+        else if (choixMenuPrincipal.equals(2)){
+            //Implémenter la fonction pour afficher les règles
+            System.out.println("Implémenter la fonction pour afficher les règles");
+        }
+        else if (choixMenuPrincipal.equals(0)){
+            System.out.println("Quitter le jeu");
+            return;
+        }
     }
 }
