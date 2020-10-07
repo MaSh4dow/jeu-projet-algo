41f4cc589f193ee6b0783358c0e7c0463b17d33b
Loïc GOURIOU <loicgouriou@MacBook-Pro-de-Loic.local>
2020-10-07 13:41:11 +0200

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // création du menu
+        Scanner scanner = new Scanner (System.in);
+        String menuJeu = new String();
+        String menuRègle = new String();
+        String menuQuitterLeJeu = new String();
+        System.out.println("1- Pour jouer écris : Jouer");
+        menuJeu = scanner.next();
+        System.out.println("2- Pour lire les règles écris : règles");
+        menuRègle = scanner.next();
+
+
     }
 }
